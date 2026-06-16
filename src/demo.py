6ad7b88b65521d567c23356bc764ddc0b84cf500
Your Name <akusilda@proton.me>
2026-06-16 22:35:00 +0300

    Создана новая ветка для домашней разработки. в добавлены новые данные, разделенные пояснениями, для того чтобы отличить, что было в первом задании, что во втором, а что осталось общим

--- src/demo.py
+++ src/demo.py
@@ -1,14 +1,24 @@
 from src.main import Product, Category
 
 if __name__ == "__main__":
+# ===================== Для общей работы ===============================
+
     product1 = Product(
         "Samsung Galaxy S23 Ultra",
         "256GB, Серый цвет, 200MP камера",
         180000.0,
         5,
     )
-    product2 = Product("Iphone 15", "512GB, Gray space", 210000.0, 8)
-    product3 = Product("Xiaomi Redmi Note 11", "1024GB, Синий", 31000.0, 14)
+    product2 = Product(
+        "Iphone 15",
+        "512GB, Gray space",
+        210000.0,
+        8)
+    product3 = Product(
+        "Xiaomi Redmi Note 11",
+        "1024GB, Синий",
+        31000.0,
+        14)
 
     print(product1.name)
     print(product1.description)
@@ -30,14 +40,29 @@ if __name__ == "__main__":
         "Смартфоны, как средство не только коммуникации, но и получения дополнительных функций для удобства жизни",
         [product1, product2, product3],
     )
-
+# ===================== Для первого задания ===============================
     print(category1.name == "Смартфоны")
     print(category1.description)
     print(len(category1.products))
     print(category1.category_count)
     print(category1.product_count)
 
-    product4 = Product('55" QLED 4K', "Фоновая подсветка", 123000.0, 7)
+# ===================== Для второго задания задания ===============================
+    print(category1.products)
+
+# ===================== Для общей работы ===============================
+    product4 = Product(
+        '55" QLED 4K',
+        "Фоновая подсветка",
+        123000.0,
+        7)
+
+# ===================== Для второго задания задания ===============================
+    category1.add_product(product4)
+    print(category1.products)
+    print(category1.product_count)
+
+# ===================== Для общей работы ===============================
     category2 = Category(
         "Телевизоры",
         "Современный телевизор, который позволяет наслаждаться просмотром, станет вашим другом и помощником",
@@ -51,3 +76,20 @@ if __name__ == "__main__":
 
     print(Category.category_count)
     print(Category.product_count)
+
+# ===================== Для второго задания задания ===============================
+    new_product = Product.new_product(
+        {"name": "Samsung Galaxy S23 Ultra", "description": "256GB, Серый цвет, 200MP камера", "price": 180000.0,
+         "quantity": 5})
+    print(new_product.name)
+    print(new_product.description)
+    print(new_product.price)
+    print(new_product.quantity)
+
+    new_product.price = 800
+    print(new_product.price)
+
+    new_product.price = -100
+    print(new_product.price)
+    new_product.price = 0
+    print(new_product.price)
