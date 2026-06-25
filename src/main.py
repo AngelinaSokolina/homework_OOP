20fe2d8ba481145dd8a681651bfaf5b28ce86b7f
Your Name <akusilda@proton.me>
2026-06-25 19:33:41 +0300

    В основной модуль main добавлены абстрактный и миксин классы

--- src/main.py
+++ src/main.py
@@ -1,4 +1,26 @@
-class Product:
+from abc import ABC,abstractmethod
+
+# Абстрактный класс
+class BaseProduct(ABC):
+
+    @abstractmethod         #Неважно какая работа, главное ее наличие
+    def work(self):
+        """Абстрактный метод, который должен быть реализован в наследниках"""
+        pass
+
+# Класс-миксин
+class MixinLog:
+    def __init__(self, *args, **kwargs):
+        """При создании объекта выводит информацию"""
+        print(repr(self))  # Вызываем __repr__ для форматирования
+        # super().__init__(*args, **kwargs)
+
+    def __repr__(self):
+        """Форматирует информацию об объекте"""
+        return f"{self.__class__.__name__}({self.name}, {self.description}, {self.price}, {self.quantity})"
+
+# Product наследует BaseProduct и MixinLog
+class Product(BaseProduct, MixinLog):
     "Класс для представления товара"
 
     name: str  # Название товара
@@ -12,6 +34,11 @@ class Product:
         self.description = description
         self.__price = price
         self.quantity = quantity
+        super().__init__(name, description, price, quantity)
+
+    def work(self):
+        """Реализация абстрактного метода"""
+        print(f"Работаем с продуктом: {self.name}")
 
     def __str__(self) -> str:
         '''Метод для отображения информации об объекте класса
